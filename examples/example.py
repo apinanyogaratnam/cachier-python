c4f47d4cad4e84d033797b059a036f6589e61cc8
apinanyogaratnam <apinanapinan@icloud.com>
2022-07-18 19:01:21 -0400

    refactor: updated example

--- examples/example.py
+++ examples/example.py
@@ -1,9 +1,13 @@
 from cachier_python import Cachier
 
+# Create a cachier instance to connect to the service
 client = Cachier('http://localhost:8000', driver='ram')
 
-client.set('key', 'value', 600)
+# cache expiry in seconds
+CACHIER_EXPIRY = 600
 
-cached_value = client.get('key')
+# set the cache
+client.set(key='my_key', value='my_value', expiry=CACHIER_EXPIRY)
 
-print('cached_value:', cached_value)
+# get the cached value
+cached_value = client.get(key='my_key')
