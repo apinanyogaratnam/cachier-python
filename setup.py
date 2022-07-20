2ba1856d9f459ec08d7d3e488fad93b550fddd2e
apinanyogaratnam <apinanapinan@icloud.com>
2022-07-19 20:44:43 -0400

    chore: updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='cachier-python',
-    version='0.2.0',
+    version='0.4.0',
     author='apinanyogaratnam',
     author_email='apinanapinan@icloud.com',
     description='A python wrapper package for connecting to a self-hosted cachier service',
